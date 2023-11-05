89c470dd798aec7058c89f664a8a44aa584bb302
Luis Fernando L <luis.lopezm@pragma.com.co>
2023-11-05 14:13:57 -0500

    finish part2

--- part2/src/main/java/org/velezreyes/quiz/question6/Drinks.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/Drinks.java
@@ -1,2 +1,10 @@
-package org.velezreyes.quiz.question6;public class Drinks {
+package org.velezreyes.quiz.question6;
+
+public class Drinks {
+    public final static String KarenTea = "KarenTea";
+    public final static String ScottCola = "ScottCola";
+    public final static double costKarenTea = 1.0;
+    public final static double costScottCola = 0.75;
+    public final static boolean karenTeaIsBuzzy = false;
+    public final static boolean ScottColaIsBuzzy = false;
 }
--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,66 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+import static org.velezreyes.quiz.question6.Drinks.KarenTea;
+import static org.velezreyes.quiz.question6.Drinks.ScottCola;
+import static org.velezreyes.quiz.question6.Drinks.costKarenTea;
+import static org.velezreyes.quiz.question6.Drinks.costScottCola;
+
+public class VendingMachineImpl implements VendingMachine{
+
+  double money = 0;
 
   public static VendingMachine getInstance() {
     // Fix me!
-    return null;
+    return new VendingMachineImpl();
+  }
+
+  @Override
+  public void insertQuarter() {
+
+    money = money + 0.25;
+  }
+
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    Drink drink = new Drink() {
+      @Override
+      public String getName() {
+        return name;
+      }
+
+      @Override
+      public boolean isFizzy() {
+        if(getName()==KarenTea){
+          return false;
+        }
+        else if (getName()== ScottCola) {
+          return true;
+        }
+        return false;
+      }
+    };
+    if(money == 0.0){
+      throw new NotEnoughMoneyException();
+    }else {
+      if(name == KarenTea){
+        if(money >= costKarenTea){
+          return  drink;
+        }else {
+          throw new NotEnoughMoneyException();
+        }
+      }
+      else if (name == ScottCola) {
+        if(money >= costScottCola){
+          return  drink;
+        }else {
+          throw new NotEnoughMoneyException();
+        }
+      }
+      else {
+        throw new UnknownDrinkException();
+      }
+
+    }
+
   }
 }
