fe370bea08715c24992331568d89b77f7d03e1c7
Luis Fernando L <luis.lopezm@pragma.com.co>
2023-11-04 20:16:09 -0500

    finish question 5

--- part1/question5.py
+++ part1/question5.py
@@ -20,7 +20,11 @@
 
 sql_create_favorite_foods = """
 
-Your SQL here.
+CREATE TABLE favorite_foods (
+      food_id integer,
+      name text ,
+      vegetarian integer 
+    );
 
 """
 
@@ -30,7 +34,8 @@ Your SQL here.
 
 sql_alter_tables_with_favorite_food = """
 
-Your SQL here.
+ALTER TABLE animals ADD COLUMN favorite_food_id integer;
+ALTER TABLE people ADD COLUMN favorite_food_id integer;
 
 """
 
@@ -40,6 +45,8 @@ Your SQL here.
 
 sql_select_all_vegetarian_pets = """
 
-Your SQL here.
+SELECT a.name , f.name FROM animals a
+INNER JOIN favorite_foods f ON a.favorite_food_id = f.food_id
+WHERE f.vegetarian = 1
 
 """
